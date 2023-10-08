a51ee62dcca120f990edd0a61c88cbe82e86947d
EC2 Default User <ec2-user@ip-172-31-45-10.us-east-2.compute.internal>
2023-10-08 08:58:38 +0000

    Remove batch break for training

--- Assignment/p0.py
+++ Assignment/p0.py
@@ -166,8 +166,6 @@ def run_epochs(epochs, lr, model, train_loader, val_loader, rank, opt_func=torch
         for batch in train_loader:
             batch_num += 1
             print(batch_num)
-            if batch_num == 2:
-                break
             images, labels = batch
             # Task 1: Complete training loop
             # Step 1 Get model prediction, i.e. run the fwd pass
